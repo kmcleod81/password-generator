c77a588d6f45c840105c091c6bd38b290f876739
kdawg <mcleod.karla@gmail.com>
2020-04-11 09:56:22 -0400

    initial setup

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+var numbers = 1;
+console.log(numbers);
+
+// var wantNumbers = confirm("Do you want it to contain numbers?");
+// console.log(wantNumbers);
+
+// var wantUpperCase = confirm("Do you want it to contain upper case?");
+// console.log(wantUpperCase);
+
+// var wantLowerCase = confirm("Do you want it to contain lower case?");
+// console.log(wantLowerCase);
