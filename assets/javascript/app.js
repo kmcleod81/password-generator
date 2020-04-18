7429d83b979da1099c1825ce5b4b1b6219634e78
kdawg <mcleod.karla@gmail.com>
2020-04-18 18:40:08 -0400

    adjuested pseudocode on JavaScript file. adjusted css file. added message for no characters selected.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,13 +13,15 @@ genButton.addEventListener("click", function () {
     var storedPassword = "";
 
     // Prompt the user for the length of password
+
     var length = 8;
     var warning = "";
+    ///check to make sure they answered yes to at least one of the questions
     for (var i = 0; i < 1; i++) {
         length = prompt(warning + "Choose your Password Length", 8);
         warning = "Invalid length. ";
         if (length < 8 || length > 128) {
-            i--;
+            i--; // telling it to go through the loop again
         }
     }
 
@@ -38,39 +40,36 @@ genButton.addEventListener("click", function () {
     // Prompt the user for what they want to include in their password
     var uselowerCase = confirm("Do you want Lowercase letter?");
 
-    var usedSC = false;
-    var usedU = false;
-    var usedN = false;
+    if (uselowerCase || useupperCase || usenumeric || usespecialChar) {
+        //in a loop, for the length of the password, pick a random item from the possible options
+        for (var i = 0; i < length; i++) {
 
+            //asking computer to pick one of the arrays to get a random character
+            var nextArrayPosition = Math.floor(Math.random() * array.length);
+            //checking to see which items user picked
+            if ((nextArrayPosition === 0 && uselowerCase) ||
+                (nextArrayPosition === 1 && useupperCase) ||
+                (nextArrayPosition === 2 && usenumeric) ||
+                (nextArrayPosition === 3 && usespecialChar)) {
+                //computer picks a good array, a random character gets generated from that array
+                var selectedArray = array[nextArrayPosition];
+                var randCharacterPosition = Math.floor(Math.random() * selectedArray.length);
+                //append the selected random character to the password displayed to the user
+                storedPassword = storedPassword + selectedArray[randCharacterPosition];
 
-    ///check to make sure they answered yes to at least one of the questions
+            } else {
+                //computer picked an array that the user didn't want to include in the password, so then stays in the same position in the loop until the computer picks one the user doesn't
+                i--;
+            }
+        };
+        // Display the results to the user
+        var passwordElement = document.getElementById("password");
+        passwordElement.innerHTML = storedPassword;
 
-
-
-
-
-
-
-
-    //in a loop, for the length of the password, pick a random item from the  possible options
-    for (var i = 0; i < length; i++) {
-
-        var nextArrayPosition = Math.floor(Math.random() * array.length);
-        if ((nextArrayPosition === 0 && uselowerCase) ||
-            (nextArrayPosition === 1 && useupperCase) ||
-            (nextArrayPosition === 2 && usenumeric) ||
-            (nextArrayPosition === 3 && usespecialChar)) {
-
-            var selectedArray = array[nextArrayPosition];
-            var randCharacterPosition = Math.floor(Math.random() * selectedArray.length);
-            storedPassword = storedPassword + selectedArray[randCharacterPosition];
-        } else {
-            i--;
-        }
-    };
-    // Display the results to the user
-    var passwordElement = document.getElementById("password");
-    passwordElement.innerHTML = storedPassword;
+    } else {
+        ///alert for no characters chosen
+        alert("No characters were chosen, click Generate Password again!")
+    }
 })
 
 
