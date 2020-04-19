f44c8bfd76f9fe5c7fccb6aca33275f4798ac5ef
kdawg <mcleod.karla@gmail.com>
2020-04-19 12:49:47 -0400

    adjusted css, and README file with updated image

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,6 +4,7 @@ var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"
 var numeric = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "?", "~"];
 
+///created new array to gather from the arrays above
 var array = [lowerCase, upperCase, numeric, specialChar];
 
 
